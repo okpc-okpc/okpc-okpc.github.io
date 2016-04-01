1950607b7bde7b05be4e24cf9bd48b0209ad1511
Dmitry <okpc.dn@gmail.com>
2016-04-01 20:06:21 +0300

    new presentation page

---
+++ script.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+	// $('#front').css('display', 'block');
+
+	$('.frontEnd').on('click', function () {
+		$('#front').css('display', 'block');
+		$(this).css('opacity', '1');
+		// $('.frontList').css('display', 'block');
+		$('#data, #back').css('display', 'none');
+		$('.dataVisualization, .backEnd').css('opacity', '.4');
+
+	});
+	$('.backEnd').on('click', function () {
+		$('#back, .backList').css('display', 'block');
+		$(this).css('opacity', '1');
+		// $('.backList').css('display', 'block');
+		$('#front, #data, .frontList, .dataList').css('display', 'none');
+		$('.frontEnd, .dataVisualization').css('opacity', '.4');
+	});
+	$('.dataVisualization').on('click', function () {
+		$('#data, .dataList').css('display', 'block');
+		$(this).css('opacity', '1');
+		// $('.dataList').css('display', 'block');
+		$('#front, #back, .frontList, .backList').css('display', 'none');
+		$('.frontEnd, .backEnd').css('opacity', '.4');
+	})
+})
